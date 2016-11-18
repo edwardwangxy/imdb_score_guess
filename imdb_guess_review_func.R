0eed1baf22d7654d5b43355abc7159d07f1a743c
Edward Wang <edwardwangxy@gmail.com>
2016-11-17 23:30:25 -0500

    finish all the functionality and create 2 small dictionary for testing

--- imdb_guess_review_func.R
+++ imdb_guess_review_func.R
@@ -1,24 +1,33 @@
 source("imdb_score_clean_func.R")
 guess_imdb_review_score <- function(test_review, termtable)
 {
+  termtable = score_9_term
+  test_review= try_score_review
+  K=nrow(termtable)
+  test_review <- paste(test_review, collapse = " ")
   clean_test_review <- imdb_score_clean_func(test_review)
   prep_fun <- tolower # makes lowercase
   tok_fun <- word_tokenizer # look at words
   lower_review_test_tok <- itoken(clean_test_review, preprocessor = prep_fun, tokenizer = tok_fun,  progressbar = FALSE) 
   reviewVocab <- create_vocabulary(lower_review_test_tok, stopwords=c(stopwords("english"),"film","movie","can","films","movies","will","scenes"))
-  inds <- which(reviewVocab$vocab$doc_counts >= sort(reviewVocab$vocab$doc_counts, decreasing=T)[K])
-  topK <- reviewVocab$vocab[inds,]
-  topK <- topK[order(topK$doc_counts,decreasing=TRUE),]
+  topK <- reviewVocab$vocab
+  topK <- subset(topK, select = c("terms","terms_counts"))
+  topK <- topK[order(topK$terms_counts,decreasing=TRUE),]
   guess_prob <- rep(NA, nrow(termtable))
+  pb3 <- txtProgressBar(min = 0, max = 9, char = "=", style = 3)
   for(i in 1:nrow(termtable)){
     term <- termtable[i,1]
     prop <- as.numeric(termtable[i,2])
     # see if the term is contained in the tweet and save the correct probability
-    if(grepl(term,demo_review)){
-      outputHil[i] <- prop
+    if(grepl(term,test_review) && length(which(topK$terms == term))>0){
+      guess_prob[i] <- prop*topK$terms_counts[which(topK$terms == term)]
     }
     else{
-      outputHil[i] <- (1-prop)
+      guess_prob[i] <- (1-prop)
     }
+    setTxtProgressBar(pb3, n)
   }
+  close(pb3)
+  final_prob = sum(log(guess_prob))
+  return(final_prob)
 }
--- main.R
+++ main.R
@@ -27,34 +27,44 @@ close(pb)
 
 #start achieving all reviews for each score level
 source("imdb_review_scraping_func.R")
-pages_each_movie = 5
+pages_each_movie = 3
 max_movies_pick = 3
-pb2 <- txtProgressBar(min = 0, max = 9, char = "=", style = 3)
+total_movie_count = 0
+progress_bar_counting = 0
+for(t in 2:9)
+{
+  link_list_name = paste("imdb_score_",n,sep="")
+  total_movie_count = total_movie_count + min(nrow(get(link_list_name)),max_movies_pick)
+}
+pb2 <- txtProgressBar(min = 0, max = total_movie_count, char = "=", style = 3)
 for(n in 2:9)
 {
   link_list_name = paste("imdb_score_",n,sep="")
   for(i in 1:min(nrow(get(link_list_name)),max_movies_pick))
   {
+    progress_bar_counting = progress_bar_counting + 1
     if(i ==1)
     {
-      review_final = c("")
+      review_final = list(NULL)
     }
     review_test = myimdb.rangereviews(get(link_list_name)$movie_imdb_link[[i]], range = pages_each_movie)
+    setTxtProgressBar(pb2, progress_bar_counting)
     review_final = c(review_final, review_test)
   }
   review_name = paste("score_",n,"_reviews", sep = "")
   assign(review_name, review_final)
   rm(list = c(link_list_name))
-  setTxtProgressBar(pb2, n)
 }
 close(pb2)
 
-#create term table for different score
+#create term table for different scores and save to dictionary
 source("imdb_score_clean_func.R")
 source("imdb_score_term_func.R")
-
+dictionary_name <- sprintf("dict%s%s.Rda",substring(Sys.time(),12,13),substring(Sys.time(),15,16))
+dict_save_location <- "dictionary"
 K_input = 30
 pb3 <- txtProgressBar(min = 0, max = 9, char = "=", style = 3)
+objects_name_to_save = c(NULL)
 for(i in 2:9)
 {
   review_name = paste("score_",i,"_reviews",sep = "")
@@ -62,10 +72,25 @@ for(i in 2:9)
   clean_reviews = imdb_score_clean_func(get(review_name))
   table = imdb_score_term_func(clean_reviews, K=K_input)
   assign(term_name, table)
-#  rm(list = c(review_name))
+  rm(list = c(review_name))
+  objects_name_to_save = c(objects_name_to_save,term_name)
   setTxtProgressBar(pb3, n)
 }
 close(pb3)
-
-
-
+save(list = objects_name_to_save,file=sprintf("%s/%s", dict_save_location,dictionary_name))
+#start guessing the score
+source("imdb_guess_review_func.R")
+test_web_url = "http://www.imdb.com/title/tt1211837/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=2240084082&pf_rd_r=1VGTP0PDM2J06YZFWVAN&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=moviemeter&ref_=chtmvm_tt_1" #<westword> imdb_url
+grab_pages = 20
+review_prob = c(NULL)
+try_score_review = myimdb.rangereviews(test_web_url, grab_pages)
+for(i in 2:9)
+{
+  i=9
+  term_name = paste("score_",i,"_term",sep="")
+  review_term_prob = guess_imdb_review_score(try_score_review, get(term_name))
+  review_prob = c(review_prob,review_term_prob)
+}
+guess_table = cbind(2:9,review_prob)
+colnames(guess_table)=c("score","prob")
+guess_table[,1][which.max(guess_table[,2])]
